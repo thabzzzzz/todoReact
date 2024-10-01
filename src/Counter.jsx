a93fdef4751d1bebd8351c89550e033a7f204803
Thabiso <tmosenohi99@gmail.com>
2024-10-01 13:21:31 +0200

    First commit

---
+++ src/Counter.jsx
@@ -0,0 +1,14 @@
+import react,{useState} from 'react';
+
+
+function Counter (){
+    const [count,setCount] = useState(0);
+return(
+    <div>
+        <button onClick={() => setCount(count + 1)}>
+            Count is {count}
+        </button>
+    </div>
+)
+}
+export default Counter
