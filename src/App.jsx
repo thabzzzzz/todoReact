d5d825ab270e550953c763ffd050c23f6250926f
Thabiso <tmosenohi99@gmail.com>
2024-10-02 11:14:40 +0200

    no message

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,54 @@
-import React from 'react';
-import './App.css';
+import { useState } from 'react';
 
-import Counter from './Counter';
+import './styles/styles.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Counter/>
+export default function App(){
+  useState('');
+  return(
+    <div>
+        <form className='new-item-form'>
+      <div className="form-row">
+      <label htmlFor="item">New item</label>
+      <input type="text" id='item' />
+      </div>
+      <button className='btn'>Add</button>
+      
+    </form>
+
+    <h1>Todo List</h1>
+    <ul className='list'>
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 1
+          </label>
+          <button className='btn btn-danger'>
+            Delete
+          </button>
+        </li>
+
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 2
+          </label>
+          <button className='btn btn-danger'>
+            Delete
+          </button>
+        </li>
+
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 3
+          </label>
+          <button className='btn btn-danger'>
+            Delete
+          </button>
+        </li>
+    </ul>
     </div>
-  );
-}
 
-export default App;
+  
+  )
+}
