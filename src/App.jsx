012d54467fd857fa56cf068d2ffaa17c1833a824
Thabiso <tmosenohi99@gmail.com>
2024-10-03 14:05:23 +0200

    Added list appending

--- src/App.jsx
+++ src/App.jsx
@@ -3,50 +3,51 @@ import { useState } from 'react';
 import './styles/styles.css';
 
 export default function App(){
-  useState('');
+  const [items, setItems]=useState([{ id: 1, text: 'juice' },{ id: 2, text: 'eggs' }]);
+  const [item, setItem]=useState('');
+
+  const [nextId, setNextId] = useState(3); // Start nextId as 3 (since the first two items have IDs 1 and 2)
   return(
     <div>
-        <form className='new-item-form'>
+       <form className='new-item-form' onSubmit={(e) => e.preventDefault()}>
       <div className="form-row">
       <label htmlFor="item">New item</label>
-      <input type="text" id='item' />
+
+      <input type="text" id='item'
+      value={item}
+      onChange={e => setItem(e.target.value)}
+      />
+
       </div>
-      <button className='btn'>Add</button>
+      <button 
+          onClick={(e) => {
+            e.preventDefault(); 
+            if (item.trim()) {
+              setItems([
+                ...items,
+                { id: nextId++, text: item }
+              ]);
+              setItem(''); 
+            }
+            setNextId(nextId + 1); 
+            setItem('');
+          }}
+          
+        >
+      Add
+      </button>
       
     </form>
 
     <h1>Todo List</h1>
-    <ul className='list'>
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item 1
-          </label>
-          <button className='btn btn-danger'>
-            Delete
-          </button>
-        </li>
+   
+   <ul>
 
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item 2
-          </label>
-          <button className='btn btn-danger'>
-            Delete
-          </button>
-        </li>
+   {items.map(item => (
+          <li key={item.id}>{item.text}</li>
+        ))}
 
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item 3
-          </label>
-          <button className='btn btn-danger'>
-            Delete
-          </button>
-        </li>
-    </ul>
+   </ul>
     </div>
 
   
